98c2ca2477129e0c70063d2c2d671eaa4829d906
Wajid Kagzi <wjkagzi@gmail.com>
2015-05-11 15:25:08 -0400

    Installed sqlite3 and created User model.

---
+++ db/migrate/20150511192415_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :user_name
+      t.string :first_name
+      t.string :last_name
+      t.text :address
+      t.string :email
+      t.string :password
+      t.string :password_confirmation
+
+      t.timestamps null: false
+    end
+  end
+end
