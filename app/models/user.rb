bf427805978bfc2e913f357556b9221ccac60489
Wajid Kagzi <wjkagzi@gmail.com>
2015-05-12 13:58:56 -0400

    Generated partial form for Sign Up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_many :owned_projects, class_name: 'Project'
+  has_many :backed_projects, class_name: 'Project'
+  has_many :pledges
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
 end
