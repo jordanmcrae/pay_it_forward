60fd11d8926d5d0583cb8853f86fdc2227186957
Wajid Kagzi <wjkagzi@gmail.com>
2015-05-13 14:04:53 -0400

    Fixed routing errors. Controllers and views work now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to(:pledges, notice: 'User was successfully created.')
+      redirect_to(:root, notice: 'User was successfully created.')
     else
       render :new
     end
