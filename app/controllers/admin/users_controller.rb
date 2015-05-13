e865e1d34e591b0760c465b15a7bf72d4b677d65
Wajid Kagzi <wjkagzi@gmail.com>
2015-05-13 12:22:10 -0400

    Initiated Admin controller and updated database.

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,8 @@
+class Admin::UsersController < ApplicationController
+  def index
+  end
+
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,13 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   resources :projects do
-    # resources :rewards
     resources :pledges
   end
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  namespace :admin do
+    resources :users
+  end
 end
