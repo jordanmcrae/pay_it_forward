40dc0a7471329e51ac23ed38166c1635a07094b4
Jordan <jordanmcrae@hotmail.com>
2015-05-12 20:35:04 -0400

    Cocoon is working... Add and remove rewards is now working correctly. Added user_id to reward table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512170629) do
+ActiveRecord::Schema.define(version: 20150513002906) do
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150512170629) do
     t.integer  "backer_limit"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "project_id"
   end
 
   create_table "users", force: :cascade do |t|
